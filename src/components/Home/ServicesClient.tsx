581886a05941a9a09e4a8ba33f6a1e652d6039b9
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-02-14 15:18:19 +0530

    update design

--- src/components/Home/ServicesClient.tsx
+++ src/components/Home/ServicesClient.tsx
@@ -30,7 +30,7 @@ export default function ServicesClient({
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
       {services.map((service, index) => {
         // Resolve the icon component safely
-        // const Icon = IconMap[service.iconName as keyof typeof IconMap] || Code2;
+        const Icon = IconMap[service.iconName as keyof typeof IconMap] || Code2;
 
         return (
           <motion.div
@@ -57,27 +57,27 @@ export default function ServicesClient({
                 className="object-cover  transition-all duration-700 group-hover:scale-105 "
                 priority={false}
               />
-              {/* <div className="absolute inset-0 bg-linear-to-t from-sky-200/90 via-sky/100 to-transparent" /> */}
+              <div className="absolute inset-0 bg-linear-to-t from-sky-200/90 via-sky/100 to-transparent" />
             </div>
 
             <div className="relative z-10 h-full p-8 flex flex-col justify-between">
-              {/* <div className="flex justify-between items-start">
+              <div className="flex justify-between items-start">
                 <div className="p-3  rounded-xl bg-brand-ice text-brand-midnight border border-brand-midnight/5 group-hover:bg-green-900 group-hover:text-white transition-colors duration-500">
                   <Icon className="w-6 h-6" />
                 </div>
                 <div className="text-brand-midnight/10 group-hover:text-green-800 transition-colors transform group-hover:rotate-45">
                   <ArrowUpRight className="w-8 h-8" />
                 </div>
-              </div> */}
+              </div>
 
-              {/* <div>
-                <h3 className="text-2xl  font-black text-brand-midnight tracking-tighter uppercase mb-2">
+              <div>
+                <h3 className="text-2xl   font-black text-brand-midnight tracking-tighter uppercase mb-2">
                   {service.title}
                 </h3>
                 <p className="text-muted-foreground text-sm font-semibold leading-relaxed">
                   {service.desc}
                 </p>
-              </div> */}
+              </div>
             </div>
 
             {/* Tactical Lighting Line */}
